3951d01d0f3e3bb6d1fe763e59acc40d3797c346
edrobi <ekdrobinski@gmail.com>
2023-04-26 15:51:36 -0500

    took all afternoon to realize I was trying to use npm start in the wrong folder.

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Counter from './Counter';
+
+
+function App() {
+  return (
+    <div>
+    <Counter />
+  </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0); //creates a state variable
+  
+    function increment() {
+        setCount(count + 1);
+      }
+      
+      function decrement() {
+        setCount(count - 1);
+      }
+
+      return (
+        <div>
+          <p>Count: {count}</p>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+        </div>
+      );
+      
+  }
+
+  export default Counter;
